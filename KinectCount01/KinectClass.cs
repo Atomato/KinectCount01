bd0f276dde9392e8fb8310aad3cf64e1d8042537
Atomato <axdwa777@gmail.com>
2017-07-13 15:38:04 +0900

    뎁스 이미지 인코딩까지 함

--- KinectCount01/KinectClass.cs
+++ KinectCount01/KinectClass.cs
@@ -13,6 +13,7 @@ namespace KinectCount01
     {
         #region Member Variables
         private KinectSensor _KinectDevice;
+        private short[] _DepthPixelData;
         private Skeleton[] _FrameSkeletons;
         public Boolean IsSkeletonFrameReady = false;
         #endregion Member Variables
@@ -29,24 +30,6 @@ namespace KinectCount01
         #endregion Constructor
 
         #region Methods
-        private void Run()
-        {
-            DiscoverKinect();
-        }
-
-        private void DiscoverKinect()
-        {
-            if (this._KinectDevice != null && this._KinectDevice.Status != KinectStatus.Connected)
-            {
-                KinectDevice = null;
-            }
-
-            if (this._KinectDevice == null)
-            {
-                KinectDevice = KinectSensor.KinectSensors.FirstOrDefault(x => x.Status == KinectStatus.Connected);
-            }
-        }
-
         /// <summary>
         /// 키넥트 status가 바뀌었을 때의 이벤트 핸들러
         /// </summary>
@@ -78,38 +61,64 @@ namespace KinectCount01
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void KinectDevice_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        private void KinectDevice_AllFramesReady(object sender, AllFramesReadyEventArgs e)
         {
-            using (SkeletonFrame frame = e.OpenSkeletonFrame())
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
             {
-                if (frame != null)
+                using (SkeletonFrame frame = e.OpenSkeletonFrame())
                 {
-                    Skeleton skeleton;
+                    DepthEncoding(KinectDevice, depthFrame);
 
-                    frame.CopySkeletonDataTo(this._FrameSkeletons);
-
-                    for (int i = 0; i < this._FrameSkeletons.Length; i++)
+                    if (frame != null)
                     {
-                        skeleton = this._FrameSkeletons[i];
+                        Skeleton skeleton;
 
-                        // 스켈레톤 트래킹 되고 있는 상태인가
-                        if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                        frame.CopySkeletonDataTo(this._FrameSkeletons);
+
+                        for (int i = 0; i < this._FrameSkeletons.Length; i++)
                         {
-                            // 모든 관절 위치 
-                            foreach (var value in Enum.GetValues(typeof(JointType)))
-                            {
-                                JointPositions[(int)value] = GetJointPoint(skeleton.Joints[(JointType)value]);
-                            }
+                            skeleton = this._FrameSkeletons[i];
 
-                            // 머리 좌표와 엉덩이 좌표로 카운팅함
-                            SquatCount.Count(JointPositions[(int)JointType.Head], JointPositions[(int)JointType.HipCenter]);
-                            IsSkeletonFrameReady = true;
+                            // 스켈레톤 트래킹 되고 있는 상태인가
+                            if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                            {
+                                // 모든 관절 위치 
+                                foreach (var value in Enum.GetValues(typeof(JointType)))
+                                {
+                                    JointPositions[(int)value] = GetJointPoint(skeleton.Joints[(JointType)value]);
+                                }
+
+                                // 머리 좌표와 엉덩이 좌표로 카운팅함
+                                SquatCount.Count(JointPositions[(int)JointType.Head], JointPositions[(int)JointType.HipCenter]);
+                                IsSkeletonFrameReady = true;
+                            }
                         }
                     }
                 }
             }
         }
 
+        private void DepthEncoding(KinectSensor kinectDevice, DepthImageFrame depthFrame)
+        {
+            if (kinectDevice != null && depthFrame != null)
+            {
+                depthFrame.CopyPixelDataTo(_DepthPixelData);
+                int playerIndex;
+                const int bitPerByte = 8;
+                byte[] encodedDepth = new byte[depthFrame.Width * depthFrame.Height / bitPerByte];
+
+                for (int i = 0; i < _DepthPixelData.Length; i++)
+                {
+                    playerIndex = _DepthPixelData[i] & DepthImageFrame.PlayerIndexBitmask;
+                    int j = i / bitPerByte;
+
+                    if (playerIndex != 0)
+                    {
+                        encodedDepth[j] |= (byte) (1 << j);
+                    }
+                }
+            }
+        }
 
         /// <summary>
         /// 스켈레톤 좌표를 뎁스 좌표로 변환
@@ -139,7 +148,9 @@ namespace KinectCount01
                     if (this._KinectDevice != null)
                     {
                         this._KinectDevice.Stop();
-                        this._KinectDevice.SkeletonFrameReady -= KinectDevice_SkeletonFrameReady;
+                        this._KinectDevice.AllFramesReady -= KinectDevice_AllFramesReady;
+                        _KinectDevice.ColorStream.Disable();
+                        _KinectDevice.DepthStream.Disable();
                         this._KinectDevice.SkeletonStream.Disable();
                         this._FrameSkeletons = null;
                     }
@@ -151,9 +162,13 @@ namespace KinectCount01
                     {
                         if (this._KinectDevice.Status == KinectStatus.Connected)
                         {
-                            this._KinectDevice.SkeletonStream.Enable();
+                            _KinectDevice.ColorStream.Enable();
+                            _KinectDevice.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+                            _KinectDevice.SkeletonStream.Enable();
+                            DepthImageStream depthStream = this._KinectDevice.DepthStream;
                             this._FrameSkeletons = new Skeleton[this._KinectDevice.SkeletonStream.FrameSkeletonArrayLength];
-                            this.KinectDevice.SkeletonFrameReady += KinectDevice_SkeletonFrameReady;
+                            _DepthPixelData = new short[depthStream.FramePixelDataLength];
+                            this.KinectDevice.AllFramesReady += KinectDevice_AllFramesReady;
                             this._KinectDevice.Start();
                         }
                     }
